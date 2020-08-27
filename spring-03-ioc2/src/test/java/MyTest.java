1d8473480fc7cfb4c54a8a63de9d4ca8da5095cd
Min Wang <ustcace@mail.ustc.edu.cn>
2020-08-27 16:00:12 +0800

    feat: 新增Spring配置文件说明

--- spring-03-ioc2/src/test/java/MyTest.java
+++ spring-03-ioc2/src/test/java/MyTest.java
@@ -5,8 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyTest {
     public static void main(String[] args) {
         // Spring容器在加载配置文件的时候，容器中管理的对象就已经初始化了，所以User2的构造函数也会被调用
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        User user = (User) context.getBean("user");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        User user = (User) context.getBean("user-alias6");
         user.show();
 
         User user2 = (User) context.getBean("user");
